fdf13e988496a07fb1debd022d187296b7e88b9f
donnymays <dtmays2@gmail.com>
2020-12-10 17:12:50 -0800

    final commit

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import Card from '../components/Card';
+import Colors from '../constants/colors';
 
 const GameOver = props => {
   return (
     <View style={styles.screen}>
-      <Text>The Game is Over</Text>
+      <Card>
+        <View>
+          <Text style={styles.statsContainerHeader}>The Game is Over</Text>
+          <Text style={styles.statsContainer}>Number of Rounds: {props.roundsNumber}</Text>
+          <Text style={styles.statsContainer}>The Number was: {props.userNumber}</Text>
+          <Button title='New Game' onPress={props.onRestart} color={Colors.titleYellow}></Button>
+        </View>
+      </Card>
     </View>
   )
 };
@@ -12,6 +21,21 @@ const GameOver = props => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: "center",
+    color: Colors.titleYellow,
+    backgroundColor: Colors.headerOrange
+  },
+  statsContainerHeader: {
+    color: Colors.backgroundBlue,
+    fontSize: 36
+  },
+  statsContainer: {
+    color: Colors.seaFoam,
+    fontSize: 22,
+    padding: 5,
+    marginVertical: 5,
+    alignItems: 'center',
     justifyContent: 'center'
   }
 });
